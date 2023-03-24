dbe7cbded952fa3c56e74a366263790ce012403b
Shubham Mishra <smdell83@gmail.com>
2023-03-24 14:30:17 +0530

    Added Atributes in Schema

--- Backend/create_poker_board/main.py
+++ Backend/create_poker_board/main.py
@@ -5,19 +5,22 @@ import hashlib
 from google.cloud import datastore
 
 def create_board_id(user_id):
-    current_time = datetime.datetime.now().strftime('%d%m%y%H%M%S')
+    current_time = datetime.datetime.now().strftime("%d%m%y")
     board_id_str = "poker_board" + user_id + current_time
-    return hashlib.md5(board_id_str.encode('utf-8')).hexdigest()
+    hash_value = hashlib.md5(board_id_str.encode('utf-8')).hexdigest()
+    return hash_value
 
 def create_poker_board(request):
     request_json = request.get_json(silent=True)
-    if not request_json or 'team_id' not in request_json or 'user_role' not in request_json or 'user_id' not in request_json:
-        return 'Status:Failure \n Error: 400 Bad Request \n Required fields : {"team_id",  "user_role", "user_id"}'
+    if not request_json or 'team_id' not in request_json or 'user_role' not in request_json or 'user_id' not in request_json or 'poker_board_type' not in request_json:
+        return 'Status:Failure \n Error: 400 Bad Request \n Required fields : {"team_id",  "user_role","poker_board_type", "user_id"}'
 
     poker_board_id = create_board_id(request_json.get('user_id'))
 
     response_dict = {
         'poker_board_id': poker_board_id,
+        'poker_board_type': request_json.get('poker_board_type'),
+        'org_id': 'cognizant',
         'created_timestamp': datetime.datetime.utcnow(),
         'last_modified_timestamp': datetime.datetime.utcnow(),
         'team_id': request_json.get('team_id'),

56295b210f47857b806bfed6b3ddb0df86792741
ankur verma <verma.ankur1729@gmail.com>
2023-03-27 12:27:50 +0530

    poker board

---
+++ Frontend/create T-shirt sizing/script2.js
@@ -0,0 +1,14 @@
+var panelsElement = document.querySelectorAll('.card')
+
+var removeActiveClasses = () => {
+    panelsElement.forEach(card => {
+        card.classList.remove('expanded');
+    });
+};
+panelsElement.forEach(card => {
+    card.addEventListener('click',() => {
+        removeActiveClasses();
+        card.classList.toggle('expanded')
+    });
+});
+   

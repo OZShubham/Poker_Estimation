d91ce6cf034e426e1fbde39bacf2dd58f8ac039e
ankur verma <verma.ankur1729@gmail.com>
2023-05-12 17:08:37 +0530

    modified ui

---
+++ website/static/create_jira_id/script.js
@@ -0,0 +1,14 @@
+function handleFileSelect(event) {
+    event.preventDefault();
+    var files = event.dataTransfer ? event.dataTransfer.files : event.target.files;
+
+    // Process the file here
+    // You can access the file using 'files[0]' and perform necessary operations
+    // For example, you can display the file name:
+    document.getElementById('file-name').textContent = files[0].name;
+  }
+
+  function handleDragOver(event) {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'copy';
+  }
